cd1e3a1697bce63962be7175f587f9290b3849a5
jvalbuena <valjavi@gmail.com>
2023-08-08 21:54:53 -0500

    m3 resulto ej 1, 2 y 3

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,6 +4,8 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return "javier"
+
 }
 
 // ⛔️ "X" e "Y" son números.
@@ -11,27 +13,35 @@ function devolverString(string) {
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x+y;
 }
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x-y;
+
 }
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x/y;
 }
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x*y;
 }
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y;
 }
+
+console.log(devolverString(),"\n",suma(5,7),resta(10,5),divide(10,2),multiplica(3,5),obtenerResto(10,3));
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
--- M03 JavaScript Fundamentos/Ejercicio 03.js
+++ M03 JavaScript Fundamentos/Ejercicio 03.js
@@ -5,37 +5,51 @@ function sonIguales(x, y) {
    // Retorna true si "x" e "y" son iguales.
    // De lo contrario, retorna false.
    // Tu código:
+   return x == y
 }
 
 function tienenMismaLongitud(str1, str2) {
    // Retorna true si los dos strings tienen la misma longitud.
    // De lo contrario, retorna false.
    // Tu código:
+   return str1.length == str2.length 
 }
 
 function menosQueNoventa(num) {
    // Retorna true si el argumento "num" es menor que noventa.
    // De lo contrario, retorna false.
    // Tu código:
+   return num < 90
 }
 
 function mayorQueCincuenta(num) {
    // Retorna true si el argumento "num" es mayor que cincuenta.
    // De lo contrario, retorna false.
    // Tu código:
-}
+   return num > 50
+} 
 
 function esPar(num) {
    // Retorna true si "num" es par.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 === 0
 }
 
 function esImpar(num) {
    // Retorna true si "num" es impar.
    // De lo contrario, retorna false.
    // Tu código:
+   return num % 2 != 0
 }
+
+console.log (sonIguales(4,4),sonIguales(3,4))
+console.log (tienenMismaLongitud("javier","ddwwee"),tienenMismaLongitud("javier","ddwee"))
+console.log (menosQueNoventa(89),menosQueNoventa(90))
+console.log (mayorQueCincuenta(51),mayorQueCincuenta(50))
+console.log (esPar(10),esPar(11))
+console.log (esImpar(11),esImpar(10))
+
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
